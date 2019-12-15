6395971bd33ec933d13aeb983cfe7a4461ac15d4
AntiMage <zhan3230@umn.edu>
2019-12-15 10:53:46 -0600

    modified PyBank with comments in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,8 @@ import os, sys
 import csv
 
 csvpath = os.path.join(r'C:\Users\Zhuoran Zhang\Desktop\budget_data.csv')
+
+# A list to store all rows from csvreader
 contents = []
 total = 0 
 average = 0
@@ -18,17 +20,20 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         contents.append(row)
         total += int(row[1])
-    for i in range(len(contents)-1):
-        change = int(contents[i+1][1])-int(contents[i][1])
-        if profit < change:
+
+    for i in range(len(contents)-1):    # There is only n-1 changes to be calculated as n values has n-1 intervals.
+        change = int(contents[i+1][1])-int(contents[i][1])  # change = next - current
+        if profit < change:             # Find the max of change and its date
             profit = change
             profit_date = contents[i+1][0]
 
-        elif loss > change:
+        elif loss > change:             # Find the min of change and its date
             loss = change
             loss_date = contents[i+1][0]
 
-    average = (int(contents[-1][1])-int(contents[0][1]))/(len(contents)-1)
+    # The summation of change will cancel out all the monthly profit/loss in between. It just simply equals to the difference
+    # between the values on the two ends.
+    average = (int(contents[-1][1])-int(contents[0][1]))/(len(contents)-1) 
 
 
 print('Financial Analysis')
@@ -47,7 +52,6 @@ with open('budget_data_results.txt', 'w') as txt_file:
     print('Financial Analysis')
     print('----------------------------')
     print(f'Total Months: {len(contents)}')
-    #print(contents)
     print('$ '+ str(total))
     print(f'$ {round(average,2)}')
 
